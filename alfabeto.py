a42221f014c05711396687b6503882755f070c46
Newton Galiza <newtonjgaliza@gmail.com>
2022-09-28 06:31:35 -0300

    Create alfabeto.py

---
+++ alfabeto.py
@@ -0,0 +1,6 @@
+letra = input() 
+
+letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+index = letras.index(letra) + 1
+
+print(f"{index}")
